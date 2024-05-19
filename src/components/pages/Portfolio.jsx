9fbc108daed660efb1ccbd6deb318365644f63ab
Mallory Howard <mallory1313@msn.com>
2024-05-19 14:59:15 -0600

    added to Portfolio css and jsx files. Added images for the projects

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -3,28 +3,57 @@ import './Portfolio.css';
 // Sample data for projects
 const projects = [
   {
-    title: 'Project 1',
-    image: '/assets/images/project1.jpg',
-    deployedUrl: 'https://example.com/project1',
-    githubUrl: 'https://github.com/yourusername/project1',
-    description: 'This is a brief description of Project 1.'
+    title: 'Tech Blog',
+    image: '/assets/images/tech-blog-logo.png',
+    deployedUrl: 'https://damp-woodland-55117-a1b32ed6e0d9.herokuapp.com/',
+    githubUrl: 'https://github.com/Meowlory3579/tech-blog',
+    description: 'MVC | Handlebars | Sequelize | Express'
   },
   {
-    title: 'Project 2',
-    image: '/assets/images/project2.jpg',
+    title: 'Time Tracker',
+    image: '/assets/images/time-tracker.png',
     deployedUrl: 'https://example.com/project2',
-    githubUrl: 'https://github.com/yourusername/project2',
-    description: 'This is a brief description of Project 2.'
+    githubUrl: 'https://github.com/Meowlory3579/time-tracker',
+    description: 'MVC | Handlebars | Sequelize | Express'
   },
-  // Add more projects as needed
+  {
+    title: 'Note Taker',
+    image: '/assets/images/note-taker.webp',
+    deployedUrl: 'https://note-taker-2fdz.onrender.com/',
+    githubUrl: 'https://github.com/Meowlory3579/note-taker',
+    description: 'Express'
+  },
+  {
+    title: 'Weather Dashboard',
+    image: '/assets/images/weather-dashboard.png',
+    deployedUrl: 'https://meowlory3579.github.io/weather-dashboard/',
+    githubUrl: 'https://github.com/Meowlory3579/weather-dashboard',
+    description: 'API | jQuery'
+  },
+  {
+    title: 'Work Day Scheduler',
+    image: '/assets/images/work-day-scheduler.jpg',
+    deployedUrl: 'https://meowlory3579.github.io/work-day-scheduler/',
+    githubUrl: 'https://github.com/Meowlory3579/work-day-scheduler',
+    description: 'jQuery'
+  },
+  {
+    title: 'Timed Coding Quiz',
+    image: '/assets/images/quiz.png',
+    deployedUrl: 'https://meowlory3579.github.io/timed-code-quiz/',
+    githubUrl: 'https://github.com/Meowlory3579/timed-code-quiz',
+    description: ''
+  },
 ];
+
 
 export default function Portfolio() {
   return (
     <div className="portfolio-container">
       {projects.map((project, index) => (
         <div className="project-card" key={index}>
-          <div className="project-image" style={{ backgroundImage: `url(${project.image})` }}>
+          <div className="project-image">
+            <img src={project.image} alt={project.title} />
             <div className="overlay">
               <h3><a href={project.deployedUrl} target="_blank" rel="noopener noreferrer">{project.title}</a></h3>
               <p>{project.description}</p>
